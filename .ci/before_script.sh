db156516c7e2a97885af255277ce6761189cda95
Geoff Johnson <geoff.johnson@coanda.ca>
2016-10-20 15:56:22 -0700

    ci: add missing dependencies

--- .ci/before_script.sh
+++ .ci/before_script.sh
@@ -7,7 +7,6 @@ set -o pipefail
 git clone https://github.com/geoffjay/libcld.git
 cd libcld
 git checkout develop
-sed -i 's/\(\[pygobject_required_version\]\,\s\[3\.\)18\(\.0\]\)/\112\2/' configure.ac
 PKG_CONFIG_PATH=./deps ./autogen.sh
 make && sudo make install
 cd ..
--- .ci/script.sh
+++ .ci/script.sh
@@ -3,6 +3,10 @@
 set -e
 set -o pipefail
 
+sed -i 's/\(\[pygobject_required_version\]\,\s\[3\.\)18\(\.0\]\)/\112\2/' configure.ac
+sed -i 's/\(\[glib_required_version\]\,\s\[2\.\)46\(\.0\]\)/\140\2/' configure.ac
+sed -i 's/\(\[peas_required_version\]\,\s\[1\.\)16\(\.0\]\)/\18\2/' configure.ac
+
 ./autogen.sh \
     --disable-ui \
     --disable-webkit \
